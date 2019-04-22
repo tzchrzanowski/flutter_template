b4427362a9d2d7ea3e5f5d0195997bcec3abdc5e
tzchrzanowski <tzchrzanowski@gmail.com>
2019-04-22 21:01:31 +0200

    set template

---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('asd');
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images'),
+        ),
+      ),
+    );
+  }
+}
